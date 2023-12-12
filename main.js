cb5aa3cd56cd483a084dd5bb703f4224fe33bced
KakouMoses <kakou.kacou.moise@gmail.com>
2023-12-12 15:07:34 +0000

    Penser à mettre des commentaires sur le programme au fur et à mesure

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { app, BrowserWindow } = require("electron")
 const path = require('node:path')
 
+// Créer une fenêtre pour exécuter le programme
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        // Les préférences web, un exemple d'utilisation des scripts de préchargement
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
@@ -14,20 +16,17 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
+// à utiliser pour lancer l'application
 app.whenReady().then(() => {
     createWindow()
 })
 
+// Fermer l'application si toutes les fenêtres sont fermées
 app.on('window-all-closed', () =>{
     if (process.platform !== 'darwin') app.quit()
 })
 
-// Histoire de savoir si tout se passait bien
-
-app.whenReady().then(() => {
-    createWindow()
-  
-    app.on('activate', () => {
-      if (BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-  })
+// Cas des MacOS, et les fenêtres actives en arrière-plan
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
--- preload.js
+++ preload.js
@@ -1,7 +1,15 @@
-const { contextBridge } = require('electron')
+// Création du script de préchargement
+const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('versions', {
     node: () => process.versions.node,
     chrome: () => process.versions.chrome,
-    electron: () => process.versions.electron
+    electron: () => process.versions.electron,
+    ping: () => ipcRenderer.invoke('ping')
 } )
+
+/* Ajout de IPC
+Note(issu du tutoriel):
+
+Remarquez comment nous enveloppons l'appel ipcRenderer.invoke('ping') dans une fonction helper plutôt que d'exposer le module ipcRenderer directement via le contextBridge. Vous ne devez jamais exposer directement l’ensemble du module ipcRenderer via le préchargement. Cela donnerait à votre moteur de rendu la possibilité d'envoyer des messages IPC arbitraires au processus principal, représentant ainsi un vecteur d'attaque puissant pour du code malveillant.
+*/
